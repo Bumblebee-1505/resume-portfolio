a3ee346be5433e62bb0a476fda99079ea43c925f
Bumblebee-1505 <akmal15051993@gmail.com>
2023-06-13 02:33:06 +0500

    Add cerificate section, css and js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,10 +35,53 @@ const observer = new IntersectionObserver((entries) => {
     });
   }, {
     threshold: 0
-  });
+  }
+);
 
   
-  document.querySelectorAll('.section').forEach(section => { observer.observe(section)} );
+document.querySelectorAll('.section').forEach(section => { observer.observe(section)} );
+
+// Portfolio
+
+const portfolioLinks = document.querySelectorAll('.portfolio__nav_link');
+ 
+portfolioLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const section = this.getAttribute('data-section');
+
+    const sections = document.querySelectorAll('.portfolio__items');
+
+    sections.forEach(sectionElement => {
+        if (sectionElement.id === section) {
+            sectionElement.classList.remove('none');
+        } else {
+            sectionElement.classList.add('none');
+        }
+        });
+    });
+});
+
+const portfolioObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      document.querySelectorAll('.portfolio__nav_link ').forEach((link) => {
+        let id = link.getAttribute('href').replace('#', '');
+        if (id === entry.target.id) {
+          link.classList.add('portfolio__active');
+        } else {
+          link.classList.remove('portfolio__active');
+        }
+      });
+    }
+  });
+}, {
+  threshold: 0
+}
+);
+
+document.querySelectorAll('.portfolio__section').forEach(section => { portfolioObserver.observe(section)} );
 
 //   Footer
 
